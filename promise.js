fc9cb0c23d9afdc630c0a32b1b6787e46501623c
SudeepTrainer <info@icenbake.com>
2023-12-07 10:02:36 +0530

    7-12-2023

--- promise.js
+++ promise.js
@@ -26,11 +26,19 @@ function getWeather(){
     return new Promise(function(resolve,reject){
        setTimeout( ()=>
        {
-        // resolve("Winter")
-        reject("There is error in fetching");
+        resolve("Winter")
+        // reject("There is error in fetching");
         }
     ,100)})
 }
+
+// getWeather().then((data)=>{
+//     console.log(`Success ${data}`);
+// },(err)=>{
+//     console.log(`Error ${err}`);
+// })
+
+
 // const weather = getWeather();
 // console.log(weather);
 function onSuccess(data){
@@ -39,5 +47,27 @@ function onSuccess(data){
 function onError(err){
     console.log(`Error ${err}`);
 }
-// getWeather().then(onSuccess,onError);
+getWeather().then(onSuccess,onError);
 getWeather().then(onSuccess).catch(onError);
+
+function getWeatherDetail(weather){
+    return new Promise(function(resolve,reject){
+        switch(weather){
+            case "Summer": resolve("Hot winds");break;
+            case "Winter": resolve("Cold winds");break;
+            default: reject("Nothing Found");
+        }
+    });
+}
+getWeather()
+.then(getWeatherDetail)
+.then(onSuccess)
+.catch(onError);
+
+fetch('https://jsonplaceholder.typicode.com/users/')
+.then(response=>{
+    return response.json();
+})
+.then(data=>{
+    console.log(data);
+})

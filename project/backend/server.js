9e35a136ae652c91464848e14894673494bdfc6a
SudeepTrainer <info@icenbake.com>
2023-12-22 12:20:17 +0530

    22-12-2023

--- project/backend/server.js
+++ project/backend/server.js
@@ -1,42 +1,56 @@
-const express = require('express');
+const express = require('express')
 // const session = require('express-session');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// const mongodbStore = require('connect-mongodb-session')(session);
+const bcrypt  = require('bcryptjs');
 const cookieParser = require('cookie-parser');
 
-const User = require('./model/user');
-const Product = require('./model/product');
-const CartItem = require('./model/cartitem');
-// create the express app and set the port
+const User = require('./models/user');
+const Product = require('./models/product');
+const CartItem = require('./models/cartitem');
+
+const databaseUri = "mongodb://127.0.0.1:27017/dbauth";
+
+async function db(){
+    await mongoose.connect(databaseUri)
+}
+
+db()
+ .then(res=>{
+    console.log("DB connected");
+ })
+ .catch(err=>{
+    console.log(`error ${err}`);
+ })
+
 const PORT = 3000;
 const application = express();
 
-const databaseUri = 'mongodb://127.0.0.1:27017/authdb'
-//connect to mongodb
-mongoose.connect(databaseUri)
-    .then(()=>{console.log(`db connected`)})
-    .catch(()=>{console.log(`Error connecting db`)})
 
-//Database schemas
-
-//middleware
-// application.use(session({
-//     secret:"secretkeytosigncokkie",
-//     resave:false,
-//     saveUninitialized:false,
-//     cookie:{
-//         secure:false,
-//         maxAge:60000,
+// const store = new mongodbStore({
+//     uri:databaseUri,
+//     collection:"sessions"
+// });
+// //middleware
+// application.use(session(
+//     {
+//         secret:"thisisasecrettosigncookie",
+//         resave:false,
+//         saveUninitialized:false,
+//         cookie:{
+//             secure:false,
+//             maxAge:60000
+//         },
+//         store:store
 //     }
-// }))
-
-application.set('view engine','ejs');
-
+// ));
 //middleware
+
 application.use(cookieParser());
 application.use(express.urlencoded({extended:true}));
 
-// middleware to check authentication status
+application.set('view engine','ejs');
+
 application.use((req,res,next)=>{
     const {auth} = req.cookies;
     if(auth){
@@ -48,96 +62,127 @@ application.use((req,res,next)=>{
 })
 const isAuthenticated = (req,res,next)=>{
     if(req.isAuthenticated){
-        next();
+        next()
     }else{
-        res.status(401).redirect('/login')
+        res.status(401).redirect("/login")
     }
 }
 //routing
-
-application.get("/",isAuthenticated,async (req,res)=>{
+application.get("/",isAuthenticated,async(req,res)=>{
     try{
         const products = await Product.find({});
-        console.log(products);
-        res.render('home',{products});
+        res.status(200).render("home",{products});
     }catch(error){
         console.log(error);
-        res.status(500).render('home',{error:"Internal server error"})
+        res.status(500).render('home',{"error":"Internal server error"})
     }
-})
-
+});
 application.get("/logout",(req,res)=>{
-    res.clearCookie('auth');
-    res.status(200).redirect('/login');
+    res.clearCookie("auth");
+    res.status(200).redirect("/login");
 })
-
-
 application.get("/login",(req,res)=>{
-    // req.session.isAuth = true;
-    // console.log(req.session);
-    // console.log(req.session.id);
-    // res.send("Home Page");
     res.render('login');
 })
 
+application.post("/login",async (req,res)=>{
+    const {username,password} = req.body;
+    try{
+        // check if the user exists
+        const user = await User.findOne({username});
+        if(user && bcrypt.compareSync(password,user.password)){
+            res.cookie("auth",true);
+            res.status(200).redirect("/");
+        }else{
+            res.status(401).render('login',{"error":"Username/password not match"})
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).render('login',{"error":"Internal server error"})
+    }
+})
 application.get("/register",(req,res)=>{
     res.render('register');
 })
-
-application.post('/register', async (req,res)=>{
+application.post('/register',async (req,res)=>{
     console.log(req.body);
     const {username,password} = req.body;
+    
     try{
-        if(!username || !password){
-            throw new Error("Enter username and password");
+        if(!username||!password){
+            throw new Error('Enter username/password')
         }
-        // username exists in db
+        // check if username already exists in database
         const existingUser = await User.findOne({username});
         if(existingUser){
-            res.status(400).render('register',{error:"Username already exists"})
-            return;
-        }   
+            res.status(400).render('register',{"error":"Username already exists"});
+            return
+        }
+        // securing the password
         const hashedPassword = bcrypt.hashSync(password,10);
+        // creating the user dta to be saved
         const newUser = new User({
             username,
             password:hashedPassword
         })
+        // saving th the database
         await newUser.save();
+        // on success redirecting to login
         res.status(201).redirect('/login');
     }catch(error){
         console.log(error);
+        res.status(500).render('register',{"error":"Internal server error"})
     }
 })
 
-application.post('/login',async (req,res)=>{
-    const {username,password} = req.body;
+// get cart
+application.get("/cart",async (req,res)=>{
     try{
-        const existingUser = await User.findOne({username});
-        if(existingUser && bcrypt.compareSync
-                (password,existingUser.password)){
-            res.cookie("auth",true);
-            res.status(201).redirect('/');
+        // get cart items from db
+        const cartItems = await CartItem.find({}).populate('product');
+        // get the total price of all items
+        const total = cartItems.reduce((acc,item)=>
+                     acc + item.quantity * item.product.price,0 )
+        res.render('cart',{cart:cartItems,total});
+    }catch(error){
+        console.log(error);
+        res.status(500).render('home',{"error":"Internal server error"})
+    }
+})
+
+// add to cart post method
+
+application.post("/addToCart",async (req,res)=>{
+    console.log(req.body);
+    try{
+        const productID = req.body.productId;
+        const quantity = parseInt(req.body.quantity);
+
+        // fetch the selected product
+        const product = await Product.findById(productID);
+        if(!product){
+            return res.status(404).send("Product not found");
         }
-
+        // check the item is already in the cart
+        const existingCartItem = await CartItem.findOne({product:productID});
+        if(existingCartItem){
+            existingCartItem.quantity +=1;
+            await existingCartItem.save();
+        }else{
+            // add a new item to the cart
+            await CartItem.create({
+                product:productID,
+                quantity:quantity
+            });
+        }
+        res.redirect("/cart");
     }catch(error){
-        console.log(error);
+        console.log('error');
+        res.status(500).render('home',{"error":"Internal server error"})
     }
 })
 
-application.get('/cart',async (req,res)=>{
-    try{
-        // get cart data from database
-        const cartItems = await CartItem.find({}).populate('Product');
-        // calculate the total items price
-        const total = cartItems.reduce((acc,item)=> 
-                            acc + item.quantity * item.product.price,0);
-        // display view cart and pass cartitems and total
-        res.status(200).render('cart',{cart:cartItems, total})
-    }catch(error){
-        console.log(error);
-        res.status(500).render('cart',{error:"Internal server error"})
-    }
-})
+//start server
 application.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on ${PORT}`);
 })
